4de3113554c2f6d4f2e07f41e2e372577199b22a
Byron Hinebaugh <bhinebaugh@gmail.com>
2020-01-24 12:08:56 -0500

    add redux as dependency, stub for store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react"
+import { Provider } from "react-redux"
+import { configureStore } from "@reduxjs/toolkit"
 import { Progress, StepContent, TotalCost, FormActions } from "./components"
+
+const store = configureStore({
+    reducer: (state) => state
+})
 
 export default function App() {
     return (
-        <>
+        <Provider store={store}>
         <Progress activeId={1} />
         <section>
             <StepContent activeId={1} />
         </section>
-        </>
+        </Provider>
     )
 }
