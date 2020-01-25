c6e1e485ee544a748b5dfb4eb8a22cf92f1186cc
Byron Hinebaugh <bhinebaugh@gmail.com>
2020-01-25 00:17:21 -0500

    pass values from state to progress sidebar

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -2,7 +2,7 @@ import React from 'react'
 import StepSummary from './StepSummary'
 import { connect } from 'react-redux'
 
-const Progress = ({ activeId, height, length, width, quantity }) => {
+const Progress = ({ activeId, height, length, width, quantity, cardboard, printQuality, selectedExtras }) => {
     return (
         <aside>
             <ul className="progress">
@@ -18,9 +18,9 @@ const Progress = ({ activeId, height, length, width, quantity }) => {
                             <dd>{quantity}</dd>
                         </dl>
                 </StepSummary>
-                <StepSummary stepId={2} stepName="Cardboard Grade" isActive={activeId === 2}>-</StepSummary>
-                <StepSummary stepId={3} stepName="Print Quality" isActive={activeId === 3}>-</StepSummary>
-                <StepSummary stepId={4} stepName="Optional Extras" isActive={activeId === 4}>-</StepSummary>
+                <StepSummary stepId={2} stepName="Cardboard Grade" isActive={activeId === 2}>{cardboard || "-"}</StepSummary>
+                <StepSummary stepId={3} stepName="Print Quality" isActive={activeId === 3}>{printQuality || "-"}</StepSummary>
+                <StepSummary stepId={4} stepName="Optional Extras" isActive={activeId === 4}>{selectedExtras.join(", ") || "-"}</StepSummary>
                 <li>
                     <a href="#total-cost" className="step step-total-cost">
                         <h3 className="step-total-cost-heading">Total Cost</h3>
@@ -39,5 +39,8 @@ export default connect(
         length: state.dimensions.length,
         width: state.dimensions.width,
         quantity: state.quantity.amount,
+        cardboard: state.cardboard,
+        printQuality: state.printQuality,
+        selectedExtras: state.selectedExtras,
     }),
 )(Progress)
